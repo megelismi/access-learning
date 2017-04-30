dafe2349158131e6149e28c0a001f31f6e5059b3
Megan Smith <meg.eli.smi@gmail.com>
2017-04-29 19:27:11 -0700

    questions component refactored, commented code cleaned up

--- client/js/components/App.js
+++ client/js/components/App.js
@@ -1,29 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions/actions";
-// import Questions from "./Questions"; 
-import { Modal } from "react-bootstrap";
+import Questions from "./Questions"; 
 
 class App extends Component {
 
   render() {
     return (
       <div className="app-container">
-      <button 
-      onClick={() => { this.props.dispatch(actions.toggleQuestionsModal()); }}
-      >
-      Open questions</button>
-        <Modal show={this.props.questionsModalOpen} onHide={() => { this.props.dispatch(actions.toggleQuestionsModal()); }}>
-      <Modal.Header className="questions-modal-header" closeButton>
-        <Modal.Title>Getting Started</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-      Hey hey hey!
-      </Modal.Body>
-      <Modal.Footer className="questions-modal-footer" />
-    </Modal>
+        <button 
+        onClick={() => { this.props.dispatch(actions.toggleQuestionsModal()); }}
+        >
+        Open questions</button>
+        <Questions 
+          showModal={this.props.questionsModalOpen}
+          hideModal={() => { this.props.dispatch(actions.toggleQuestionsModal()); }}
+        />
       </div>
-    ); 
+    );
   }
 }
 
@@ -33,7 +27,3 @@ const mapStateToProps = state => ({
 
 export default connect(mapStateToProps)(App);
 
-  // <Questions 
-  //         showModal={this.props.questionsModalOpen}
-  //         hideModal={() => { this.props.dispatch(actions.toggleQuestionsModal()); }}
-  //       />
