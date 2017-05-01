0cf34b7231fa6d5b0cfe7f2d1702e14f44143a7d
Megan Smith <meg.eli.smi@gmail.com>
2017-05-01 07:11:33 -0700

    beginning of CSS styling

--- client/js/components/Questions.js
+++ client/js/components/Questions.js
@@ -2,15 +2,13 @@ import React from "react";
 import { Modal } from "react-bootstrap";
 
 const Questions = (props) => (
-    <Modal show={props.showModal} onHide={props.hideModal}>
+    <Modal className="questions-modal" show={props.showModal} onHide={props.hideModal}>
       <Modal.Header className="questions-modal-header" closeButton>
-        <Modal.Title>Getting Started</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <div className="questions-container">{props.question} </div>
         {props.answer}
       </Modal.Body>
-      <Modal.Footer className="questions-modal-footer" />
     </Modal>
   );
 
