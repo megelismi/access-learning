6c8088a768f8bd79ca1fee8ae09e34487018e0c9
Megan Smith <meg.eli.smi@gmail.com>
2017-04-30 18:47:22 -0700

    questions being pulled from database, put in redux store and rendered to the screen

--- client/js/actions/actions.js
+++ client/js/actions/actions.js
@@ -3,3 +3,30 @@ export const TOGGLE_QUESTIONS_MODAL = "TOGGLE_QUESTIONS_MODAL";
 export const toggleQuestionsModal = () => ({
   type: TOGGLE_QUESTIONS_MODAL
 });
+
+export const GET_ALL_QUESTIONS_SUCCESS = "GET_ALL_QUESTIONS_SUCCESS";
+export const getAllQuestionsSuccess = questions => ({
+  type: GET_ALL_QUESTIONS_SUCCESS, 
+  questions
+});
+
+export const GET_ALL_QUESTIONS_ERROR = "GET_ALL_QUESTIONS_ERROR";
+export const getAllQuestionsError = error => ({
+  type: GET_ALL_QUESTIONS_ERROR, 
+  error
+});
+
+export const getAllQuestions = () => dispatch => {
+  return fetch("/questions")
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(res.statusText); 
+    }
+    return res.json(); 
+  }).then(questions => {
+    dispatch(getAllQuestionsSuccess(questions)); 
+  }).catch(error => {
+    dispatch(getAllQuestionsError(error)); 
+  });
+};
+
--- client/js/components/App.js
+++ client/js/components/App.js
@@ -1,11 +1,59 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions/actions";
+import Feedback from "./Feedback";
 import Questions from "./Questions"; 
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state={
+      showFeedback: false
+    }
+  }
+
+  componentWillMount() {
+    this.props.dispatch(actions.getAllQuestions());
+  }
+
+  openNextQuestion() {
+    this.props.dispatch(actions.toggleQuestionsModal());
+  }
+
+  checkAnswer (e) {
+    e.preventDefault();
+    console.log(this.answer.value);
+    this.props.dispatch(actions.toggleQuestionsModal());
+    this.setState({
+      showFeedback: true
+    });
+    setTimeout(this.openNextQuestion.bind(this), 3000);
+  }
   render() {
+    console.log('state feedback', this.state.showFeedback);
+    let question; 
+    if (this.props.selectedQuestion === undefined) {
+      question = (
+        <div />
+      ); 
+    } else {
+      question = this.props.selectedQuestion.question;
+    }
+
+    let blockOrNone = this.state.showFeedback ? "block" : "none";
+    console.log(blockOrNone);
+    const showOrNot = {
+      display: blockOrNone
+    }
+
+    const answerForm = (
+      <form className="answer-form" onSubmit={this.checkAnswer.bind(this)}>
+        <input ref={answer => this.answer = answer} />
+        <button type="submit">Submit</button>
+      </form>
+    );
+
     return (
       <div className="app-container">
         <button 
@@ -15,6 +63,11 @@ class App extends Component {
         <Questions 
           showModal={this.props.questionsModalOpen}
           hideModal={() => { this.props.dispatch(actions.toggleQuestionsModal()); }}
+          question={question}
+          answer={answerForm}
+        />
+        <Feedback
+          showOrNot={showOrNot}
         />
       </div>
     );
@@ -22,7 +75,9 @@ class App extends Component {
 }
 
 const mapStateToProps = state => ({
-    questionsModalOpen: state.questionsModalOpen
+    questionsModalOpen: state.questionsModalOpen, 
+    questions: state.questions, 
+    selectedQuestion: state.selectedQuestion
   });
 
 export default connect(mapStateToProps)(App);
